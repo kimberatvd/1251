36d041ae33ac7fd9c30de05af55ef11e56914a3f
Kimber <kimber@visualdialogue.com>
2014-09-25 11:54:18 -0400

    adjust spacing for small screens
    adjust spacing so homepage animation displays correctly on smaller
    screens

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -7,6 +7,7 @@
   <meta name="description" content="<?php echo html($site->description()) ?>" />
   <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
   <meta name="robots" content="index, follow" />
+  <meta name="viewport" content="initial-scale=1" />
 
   <?php echo css('assets/styles/styles.css') ?>
   <?php echo css('assets/styles/responsive-styles.css') ?>
--- site/templates/home.php
+++ site/templates/home.php
@@ -3,7 +3,7 @@
 <section class="home">
 
   <article>
-    <img src="/assets/images/logo.png" />
+    <img src="/1251/assets/images/logo.png" /><!--remove /1251 when not in subfolder-->
     <?php snippet('menu') ?>
   </article>
 
