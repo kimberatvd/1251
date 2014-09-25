29139e2009fe8f42a64a01da669bb7eafe2cd236
Kimber <kimber@visualdialogue.com>
2014-09-25 13:55:23 -0400

    turn off animation on mobile
    put jquery function in an if statement that detects the user’s screen
    size and only animates if the screen is less than 500px wide.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,22 +1,15 @@
 <script>
-$(window).scroll(function(i){
-    var scrollVar = $(window).scrollTop();
-    $('.home').css({'top': .7*scrollVar });
-    $('.home').css({'opacity':( 100-scrollVar )/100});
-})
 
+var myWindowSize = $(window).width();
 
-// var target = $('.home');
-// var targetHeight = target.outerHeight();
+if (myWindowSize >= 500) { //if theyre not on mobile run the following script
+	$(window).scroll(function(i){
+	    var scrollVar = $(window).scrollTop();
+	    $('.home').css({'top': .7*scrollVar });
+	    $('.home').css({'opacity':( 100-scrollVar )/100});
+	})
+}
 
-// $(document).scroll(function(e){
-//     var scrollPercent = (targetHeight - window.scrollY)  / targetHeight;
-//     if(scrollPercent >= 0){
-//         target.css('opacity', scrollPercent);
-//     } else if (scrollPercent > 90) {
-//     	target.css('opacity', 0);
-//     }
-// });
 </script>
 
 </body>
