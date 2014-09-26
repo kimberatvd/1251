3cf908f3d6c8398a3ef348bbbc7560ccc5f0b871
Kimber <kimber@visualdialogue.com>
2014-09-26 10:26:39 -0400

    homepage alignment
    updated homepage so it was the full height of the window, logo is
    vertically and horizontally centered, and menu is snapped to bottom of
    window.

--- site/templates/home.php
+++ site/templates/home.php
@@ -3,7 +3,10 @@
 <section class="home">
 
   <article>
-    <img src="/1251/assets/images/logo.png" /><!--remove /1251 when not in subfolder-->
+    <div class="logo">
+    	<img src="/1251/assets/images/logo.png" /><!--remove /1251 when not in subfolder-->
+    </div>
+
     <?php snippet('menu') ?>
   </article>
 
