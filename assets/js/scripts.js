22c6d9b5ad84f7e9d602a5f896b3d82340be8283
Kimber <kimber@visualdialogue.com>
2014-09-29 11:07:18 -0400

    Revert e44f92d..2ec92a0
    This rolls back to commit e44f92d35f1865ed34e787826c8b7e12aa025d31.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,6 @@
 var myWindowSize = $(window).width();
-var myWindowHeight = $(window).height();
 
-function isiPad(){
-    return (navigator.platform.indexOf("iPad") != -1);
-}
-
-
-if(!isiPad() && myWindowSize >= 500) {
-
+if (myWindowSize >= 500) { //if theyre not on mobile run the following script
 	$(window).scroll(function(i){
 	    var scrollVar = $(window).scrollTop();
 	    $('.home').css({'top': .7*scrollVar });
@@ -19,7 +12,7 @@ $('.hamburger').click(function(){
 	$('nav').slideToggle('slow');
 });
 
-if (myWindowSize <=725) {
+if (myWindowSize <=500) {
 	$('nav ul li').click(function(){
 		$('nav').hide();	
 	});
@@ -33,4 +26,3 @@ if (myWindowSize <=725) {
 	    }
 	});
 }
-
